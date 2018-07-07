589cff85dc08e757278779d02ea4f0cd46e807a5
irina2510 <cryzhanowskaya.ira@gmail.com>
2018-07-07 20:33:37 +0300

    test.ua

---
+++ test.ua/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Пробуем</title>
+    <style>
+        h1 { color: #a6780a; font-weight: normal; }
+        h2 {color: olive;border-bottom: 2px solid black;}
+    </style>
+</head>
+<body>
+
+<h1> пример1</h1>
+<h2>Заголовок 2</h2>
+<p>Первый абзац.</p>
+<p>Второй абзац.</p>
+</body>
+</html>
