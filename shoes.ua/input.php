c642402b3b0efe370b5c689a6d20a9a7b793e5dc
irina2510 <cryzhanowskaya.ira@gmail.com>
2018-07-08 08:31:02 +0300

    git hab

---
+++ shoes.ua/input.php
@@ -0,0 +1,33 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Test script</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+            integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+            integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+            crossorigin="anonymous"></script>
+</head>';
+echo "Форма отправлена $_POST[form] ";
+echo "<p><b>Сезон:</b><Br>";
+echo "$_POST[Choice1]<br>";
+echo "$_POST[Choice2]<br>";
+echo "<p><b>Пол:</b><Br>";
+echo "$_POST[Choice3]<br>";
+echo "$_POST[Choice4]<br>";
+echo "<p><b>Категории:</b><Br> ";
+echo "$_POST[select]<br>";
+echo "<p><b>Размер:</b><Br>";
+echo "$_POST[Question]<br>";
+echo "<p><b>Контактная информация:</b><Br>";
+?>
